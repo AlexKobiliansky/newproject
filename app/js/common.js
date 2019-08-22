4640ef4c8f6024084bb7fc83b44fd1458ec34f19
alex <alex_kobilianskiy@bigmir.net>
2019-08-22 19:15:18 +0300

    section contacts complete

--- app/js/common.js
+++ app/js/common.js
@@ -340,3 +340,202 @@ $(document).ready(function(){
     //===== /geography =====//
 
 });
+
+
+
+
+
+$(window).on('load', function(){
+    if ($('#dark-map').length) {
+        function initMap(){
+            var mapAttr = $('#dark-map'),
+                latitude = mapAttr.data('lat'),
+                longitude = mapAttr.data('lng'),
+                zoom = mapAttr.data('zoom'),
+                image = mapAttr.data('marker'),
+                location = {lat: latitude, lng: longitude};
+
+            var map = new google.maps.Map(document.getElementById('dark-map'), {
+                center: location,
+                zoom: zoom,
+                disableDefaultUI: true,
+                zoomControl: true,
+                streetViewControl: true,
+                fullscreenControl: true,
+
+                styles: [
+                    {
+                        featureType: "all",
+                        elementType: "labels.text.fill",
+                        stylers: [
+                            {
+                                saturation: 36
+                            },
+                            {
+                                color: "#000000"
+                            },
+                            {
+                                lightness: 40
+                            }]
+                    }, {
+                        featureType: "all",
+                        elementType: "labels.text.stroke",
+                        stylers: [{
+                            visibility: "on"
+                        }, {
+                            color: "#000000"
+                        }, {
+                            lightness: 16
+                        }]
+                    }, {
+                        featureType: "all",
+                        elementType: "labels.icon",
+                        stylers: [{
+                            visibility: "off"
+                        }]
+                    }, {
+                        featureType: "administrative",
+                        elementType: "geometry.fill",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 20
+                        }]
+                    }, {
+                        featureType: "administrative",
+                        elementType: "geometry.stroke",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 17
+                        }, {
+                            weight: 1.2
+                        }]
+                    }, {
+                        featureType: "landscape",
+                        elementType: "all",
+                        stylers: [{
+                            visibility: "on"
+                        }]
+                    }, {
+                        featureType: "landscape",
+                        elementType: "geometry",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 20
+                        }]
+                    }, {
+                        featureType: "landscape",
+                        elementType: "labels.icon",
+                        stylers: [{
+                            saturation: "-100"
+                        }, {
+                            lightness: "-54"
+                        }]
+                    }, {
+                        featureType: "poi",
+                        elementType: "all",
+                        stylers: [{
+                            visibility: "on"
+                        }, {
+                            lightness: "0"
+                        }]
+                    }, {
+                        featureType: "poi",
+                        elementType: "geometry",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 21
+                        }]
+                    }, {
+                        featureType: "poi",
+                        elementType: "labels.icon",
+                        stylers: [{
+                            saturation: "-89"
+                        }, {
+                            lightness: "-55"
+                        }]
+                    }, {
+                        featureType: "road",
+                        elementType: "labels.icon",
+                        stylers: [{
+                            visibility: "off"
+                        }]
+                    }, {
+                        featureType: "road.highway",
+                        elementType: "geometry.fill",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 17
+                        }]
+                    }, {
+                        featureType: "road.highway",
+                        elementType: "geometry.stroke",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 29
+                        }, {
+                            weight: 0.2
+                        }]
+                    }, {
+                        featureType: "road.arterial",
+                        elementType: "geometry",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 18
+                        }]
+                    }, {
+                        featureType: "road.local",
+                        elementType: "geometry",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 16
+                        }]
+                    }, {
+                        featureType: "transit",
+                        elementType: "geometry",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 19
+                        }]
+                    }, {
+                        featureType: "transit.station",
+                        elementType: "labels.icon",
+                        stylers: [{
+                            visibility: "on"
+                        }, {
+                            saturation: "-100"
+                        }, {
+                            lightness: "-51"
+                        }]
+                    }, {
+                        featureType: "water",
+                        elementType: "geometry",
+                        stylers: [{
+                            color: "#000000"
+                        }, {
+                            lightness: 17
+                        }]
+                    }]
+
+            });
+
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: image,
+            });
+
+            marker.setMap(map);
+        }
+
+        initMap();
+    }
+});
